d032963bdccfdf0989079bfb05ffc64de5ee9083
randolrs <r.shane.randolph@gmail.com>
2016-08-27 14:04:52 -0500

    real

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825144139) do
+ActiveRecord::Schema.define(version: 20160827185218) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20160825144139) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.text     "about",                  default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
