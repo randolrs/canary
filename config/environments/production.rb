1731a224f4f507ff56d5d4519da0038cc5876298
randolrs <r.shane.randolph@gmail.com>
2016-09-22 15:11:04 -0500

    fixed test email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => "legalinsiderinformation.com",
+   :user_name            => "legalinsiderinformation@gmail.com",
    :password             => "Secure10",
    :authentication       => "plain",
   :enable_starttls_auto => true
