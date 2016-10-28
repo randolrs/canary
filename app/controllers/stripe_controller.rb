e6556d127abcbd5c4f3310ad096553533c3f308a
randolrs <r.shane.randolph@gmail.com>
2016-10-28 18:25:15 -0500

    remove conditions for test

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -8,11 +8,13 @@ class StripeController < ApplicationController
     # Retrieve the request's body and parse it as JSON
     event_json = JSON.parse(request.body.read)
 
-    event = Stripe::Event.retrieve(event_json["id"])
+    # validation for live mode event = Stripe::Event.retrieve(event_json["id"])
 
     stripe_event = StripeEvent.new
 
-    stripe_event.update(:stripe_id => event.id)
+    #stripe_event.update(:stripe_id => event.id)
+
+    stripe_event.update(:stripe_id => 21)
 
     stripe_event.save
 
