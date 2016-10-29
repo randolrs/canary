e4013195454c643351c4a07fd5ef138e0d695c7c
randolrs <r.shane.randolph@gmail.com>
2016-10-28 19:05:57 -0500

    parse type

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -6,22 +6,41 @@ class StripeController < ApplicationController
   
 
     # Retrieve the request's body and parse it as JSON
+
     event_json = JSON.parse(request.body.read)
 
-    # validation for live mode event = Stripe::Event.retrieve(event_json["id"])
+    event = Stripe::Event.retrieve(event_json["id"])
 
-    stripe_event = StripeEvent.new
+    StripeEvent.create(id: event.id)
+
+    if event.type == "invoice.payment_failed"
+
+      if StripeCustomer.where(:stripe_customer_id => event.data.object.customer).exists?
+
+        stripe_customer = StripeCustomer.where(:stripe_customer_id => event.data.object.customer).last
+
+        if User.where(stripe_customer.user_id).exists?
+          
+          user = User.find(stripe_customer.user_id)
+
+          user.update(:billing_active => false)
+
+        end
+
+      end
+
+    end
 
     #stripe_event.update(:stripe_id => event.id)
 
-    stripe_event.update(:stripe_id => event_json["data"]["object"]["id"])
+    #stripe_event.update(:stripe_id => event_json["data"]["object"]["id"])
 
-    stripe_event.save
+    #stripe_event.save
 
     head :ok
 
 
-    rescue Stripe::CardError => e
+    rescue
       head :internal_server_error
     end
 
