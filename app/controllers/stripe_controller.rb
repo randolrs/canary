b68a658ca7b0f0577b76011997525ea4b6c860da
randolrs <r.shane.randolph@gmail.com>
2016-10-28 21:13:36 -0500

    charm

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -2,28 +2,43 @@ class StripeController < ApplicationController
 
     protect_from_forgery :except => :webhook
 
-    Stripe.api_key = "sk_test_xyxH2ODPhyoDwYtnrtPsPRYK"
-
     def webhook
 
     # Retrieve the request's body and parse it as JSON
 
     event_json = JSON.parse(request.body.read)
 
+
+    if event_json['livemode'] == false
+
+      Stripe.api_key = "sk_test_xyxH2ODPhyoDwYtnrtPsPRYK"
+
+      event = Stripe::Event.retrieve("evt_00000000000000")
+
+
+    else
+
+      event = Stripe::Event.retrieve(event_json["id"])
+
+    end
+
+    stripe_event_id = event.id
+
+    stripe_customer_id = event.data.object.customer
+
+    
     #event = Stripe::Event.retrieve(event_json["id"])
 
     #event = Stripe::Event.retrieve("evt_00000000000000") #test only
 
     stripe_event = StripeEvent.new
 
-    stripe_event.update(:stripe_id => event_json['id'], :stripe_customer_id => event_json['data']['object']['customer'])
-
-    #stripe_event.update(:id => "tester_time_666")
+    stripe_event.update(:stripe_id => stripe_event_id, :stripe_customer_id => stripe_customer_id)
 
     stripe_event.save
 
 
-    if event_json["type"] == "invoice.payment_failed"
+    if event.type == "invoice.payment_failed"
 
     #   if StripeCustomer.where(:stripe_customer_id => event.data.object.customer).exists?
 
