1a26cd51dd28af38269b74789de54494d0dd9802
randolrs <r.shane.randolph@gmail.com>
2016-10-28 22:00:06 -0500

    test

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -14,81 +14,90 @@ class StripeController < ApplicationController
 
 
 
-    def webhook
+  def webhook
 
     # Retrieve the request's body and parse it as JSON
 
     event_json = JSON.parse(request.body.read)
 
 
+
+
     if event_json['livemode'] == false
+
+
 
       stripe_event_id = event_json["id"]
 
-      stripe_customer_id = event_json["data"]["object"]["customer"]
-
       live = false
 
+
+
+
     else
+
+
 
       event = Stripe::Event.retrieve(event_json["id"])
 
       stripe_event_id = event.id
 
-      stripe_customer_id = event.data.object.customer
-
       live = true
 
-      if StripeUserCustomer.where(:stripe_customer_id => stripe_customer_id).exists?
-        
-        stripe_user_customer = StripeUserCustomer.where(:stripe_customer_id => stripe_customer_id).last
-
-        if User.where(:id => stripe_user_customer.user_id).exists?
-
-          user = User.where(:id => stripe_user_customer.user_id).last
 
 
+      if event.type == "invoice.payment_failed"
 
-          if event.type == "invoice.payment_failed"
+        stripe_customer_id = event.data.object.customer
 
+        if StripeUserCustomer.where(:stripe_customer_id => stripe_customer_id).exists?
+    
+          if User.where(:id => stripe_user_customer.user_id).exists?
 
+            user = User.where(:id => stripe_user_customer.user_id).last
 
-          elsif event.type == "invoice.payment_succeeded"
-
-
+            user.update(:billing_active => false)
 
           end
 
-
-
-
         end
 
+      elsif event.type == "invoice.payment_succeeded"
+
+        stripe_customer_id = event.data.object.customer
+
+        if StripeUserCustomer.where(:stripe_customer_id => stripe_customer_id).exists?
+    
+          if User.where(:id => stripe_user_customer.user_id).exists?
+
+            user = User.where(:id => stripe_user_customer.user_id).last
+
+            user.update(:billing_active => true)
+
+          end
+
+        end
 
       end
 
 
+
+
+
     end
+
+
 
     stripe_event = StripeEvent.new
 
-    stripe_event.update(:stripe_id => stripe_event_id, :stripe_customer_id => stripe_customer_id, :live => live)
+    stripe_event.update(:stripe_id => stripe_event_id, :live => live)
 
     stripe_event.save
 
-    
-
-    #stripe_event.update(:stripe_id => event.id)
-
-    #stripe_event.update(:stripe_id => event_json["data"]["object"]["id"])
-
-    #stripe_event.save
-
     head :ok
-    #Stripe.api_key = ENV['STRIPE_SECRET_KEY_LIVE']
 
     # rescue
     #   head :conflict
-    end
+  end
 
 end
